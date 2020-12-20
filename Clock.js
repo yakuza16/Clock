4cae8ddaa6e41d3217fe24c90c74f73ace0126ff
Marcin Spławiński <64729676+yakuza16@users.noreply.github.com>
2020-12-20 10:56:15 +0100

    feat clock is working

--- Clock.js
+++ Clock.js
@@ -6,17 +6,23 @@ class Clock {
       secondsId: "seconds",
     };
 
-    this.currentHours = new Date().getHours();
-    this.currentMinutes = new Date().getMinutes();
-    this.currentSeconds = new Date().getSeconds();
-
+    this.currentHours = null;
+    this.currentMinutes = null;
+    this.currentSeconds = null;
     this.hoursHand = document.getElementById(clockElementsIds.hoursId);
     this.minutesHand = document.getElementById(clockElementsIds.minutesId);
     this.secondsHand = document.getElementById(clockElementsIds.secondsId);
 
+    this.getActualTime();
     this.initializeClock();
+    this.startTime();
+  }
 
-    console.log(this.currentHours);
+  getActualTime() {
+    const currentFullTime = new Date();
+    this.currentHours = currentFullTime.getHours();
+    this.currentMinutes = currentFullTime.getMinutes();
+    this.currentSeconds = currentFullTime.getSeconds();
   }
 
   initializeClock() {
@@ -31,5 +37,40 @@ class Clock {
     this.hoursHand.style.transform = `rotate(calc(180deg + ${
       this.currentHours * 30 + this.currentMinutes / 2
     }deg ))`;
+
+    console.log(this.secondsHand.style.transform, this.currentSeconds);
+  }
+
+  startTime() {
+    setInterval(() => {
+      if (this.currentSeconds >= 60) {
+        this.currentSeconds = 0;
+        this.currentMinutes++;
+        this.minutesHand.style.transform = `rotate(calc(180deg + ${
+          this.currentMinutes * 6
+        }deg ))`;
+        if (this.currentMinutes >= 60) {
+          this.currentMinutes = 0;
+          this.currentHours++;
+          this.hoursHand.style.transform = `rotate(calc(180deg + ${
+            this.currentHours * 30 + this.currentMinutes / 2
+          }deg ))`;
+          if (this.currentHours >= 12) {
+            this.currentHours = 0;
+            this.hoursHand.style.transform = `rotate(calc(180deg + ${
+              this.currentHours * 30 + this.currentMinutes / 2
+            }deg ))`;
+          }
+        }
+      }
+      this.currentSeconds++;
+      this.secondsHand.style.transform = `rotate(calc(180deg + ${
+        this.currentSeconds * 6
+      }deg ))`;
+
+      console.log(this.currentSeconds);
+      console.log(this.currentMinutes);
+      console.log(this.currentHours);
+    }, 1000);
   }
 }
